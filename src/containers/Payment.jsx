4b59b529e3bb78dbb30449194878b8f47e33da05
Thomas Bernal <thhomasgt99@gmail.com>
2022-05-19 10:55:28 -0500

    check point

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../styles/components/Payment.css'
 
 const Payment = () => {
 	return (
-		<div>Payment</div>
+		<div className="Payment">
+			<div className="Payment-content">
+				<h3>Resument del pedido:</h3>
+				<div className="Payment-button">
+					Boton de pago con Paypal 
+				</div>
+			</div>
+			<div></div>
+		</div>
 	)
 }
 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../styles/components/Success.css'
 
 const Success = () => {
 	return (
-		<div>Success</div>
+		<div className="Success">
+			<div className="Success">
+				<h2>Oscar, Gracias por tu compra</h2>
+				<span>Tu pedido llegara en 3 dias a tu direccion:</span>
+				<div className="Success-map">
+					Google Maps
+				</div>
+			</div>
+		</div>
 	)
 }
 
